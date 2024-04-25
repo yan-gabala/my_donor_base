d06206d8d9f2c222fb8ad164f94e7ff52ea57c45
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-04-26 00:42:53 +0300

    Изменения в настройке Cloudpayment

--- backend/cloudpayments/models.py
+++ backend/cloudpayments/models.py
@@ -1,8 +1,5 @@
-from decimal import Decimal
-
 from django.db import models
 
-from payments import PurchasedItem
 from donations.models import Donation
 
 
@@ -14,41 +11,13 @@ class CloudPayment(Donation):
     payment_status = models.CharField(
         max_length=20,
         choices=[
-            ("pending", "Ожидание"),
             ("success", "Успешно"),
             ("failure", "Ошибка")
         ],
-        default="pending",
         verbose_name="Статус платежа"
     )
 
     class Meta:
-        ordering = ("-pub_date",)
+        ordering = ("-pub_date", "payment_status")
         verbose_name = "Пожертвование Cloudpayment"
         verbose_name_plural = "Пожертвования Cloudpayment"
-
-    def get_failure_url(self):
-        """
-        Возвращает URL, на который будет перенаправлен пользователь
-        при неудачном платеже.
-        """
-        return f"http://example.com/payments/{self.pk}/failure"
-
-    def get_success_url(self):
-        """
-        Возвращает URL, на который будет перенаправлен пользователь
-        при успешнои платеже.
-        """
-        return f"http://example.com/payments/{self.pk}/success"
-
-    def get_purchased_items(self):
-        """
-        Возвращает атрибуты платежа.
-        """
-        yield PurchasedItem(
-            name="Платеж Cloudpayment",
-            sku="СloudPayment",
-            quantity=1,
-            price=Decimal(self.donat or self.custom_donat),
-            currency="RUB",
-        )
--- backend/cloudpayments/views.py
+++ backend/cloudpayments/views.py
@@ -1,7 +1,3 @@
-import datetime
-import requests
-
-from django.conf import settings
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
@@ -20,42 +16,16 @@ class CloudPaymentsViewSet(ViewSet):
         Создание экзепмляра Cloudpayment.
         """
         amount = request.data.get("amount")
-        url = "https://api.cloudpayments.ru/payments/tokens/create"
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Basic {settings.CLOUDPAYMENTS_PUBLIC_KEY}:"
-                             f"{settings.CLOUDPAYMENTS_SECRET_KEY}"
-        }
-        data = {
-            "Amount": amount,
-            "Currency": "RUB",
-            "AccountId": settings.CLOUDPAYMENTS_ACCOUNT_ID,
-            "Email": request.user.email,
-            "Description": "Денежный перевод",
-            "JsonData": {
-                "OrderId": self.generate_unique_order_id()
-            }
-        }
-        response = requests.post(url, headers=headers, json=data)
-        if response.status_code == 200:
-            payment_token = response.json().get("Model").get("Token")
-            cloudpayments_instance = CloudPayment(
-                user=request.user,
-                amount=amount,
-                currency="RUB",
-                order_id=data["JsonData"]["OrderId"],
-                payment_token=payment_token
-            )
-            cloudpayments_instance.save()
-            serializer = CloudpaymentsSerializer(cloudpayments_instance)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"error": "Ошибка при создании платежа"},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    @staticmethod
-    def generate_unique_order_id():
-        """
-        Создание идентификатора перевода.
-        """
-        return datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        payment_status = request.data.get("payment_status")
+        cloudpayments_instance = CloudPayment(
+            payment_status=payment_status,
+            email=request.user.email,
+            donat=amount,
+            currency="RUB",
+            payment_method="Cloudpayments",
+            monthly_donat=False,
+            subscription="",
+        )
+        cloudpayments_instance.save()
+        serializer = CloudpaymentsSerializer(cloudpayments_instance)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
