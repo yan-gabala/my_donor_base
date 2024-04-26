8d488e77a88ab602982fd391b8c3754b29682473
jisdtn <122818010+jisdtn@users.noreply.github.com>
2024-04-26 12:52:44 +0200

    add unisender vars to settings

--- backend/donor_base/settings.py
+++ backend/donor_base/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = os.getenv('DEBUG', '').lower() in ['true', 'yes', '1']
+DEBUG = os.getenv("DEBUG", "").lower() in ["true", "yes", "1"]
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",
     "donations.apps.DonationsConfig",
-    "contacts.apps.DonationsConfig",
+    "contacts.apps.ContactsConfig",
 ]
 
 MIDDLEWARE = [
@@ -65,28 +65,28 @@ WSGI_APPLICATION = "donor_base.wsgi.application"
 
 # postgresql
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "django"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa: E501
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa: E501
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa: E501
     },
 ]
 
@@ -101,8 +101,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",  # noqa: E501
+    "PAGE_SIZE": 3,
 }
 
 STATIC_URL = os.getenv("STATIC_URL", "/static/")
@@ -132,16 +132,19 @@ FIVE_HUNDRED = 500
 THOUSAND = 1000
 THREE_THOUSAND = 3000
 AMOUNT = [
-    (ZERO, '0'),
-    (THREE_HUNDRED, '300'),
-    (FIVE_HUNDRED, '500'),
-    (THOUSAND, '1000'),
-    (THREE_THOUSAND, '3000')
+    (ZERO, "0"),
+    (THREE_HUNDRED, "300"),
+    (FIVE_HUNDRED, "500"),
+    (THOUSAND, "1000"),
+    (THREE_THOUSAND, "3000"),
 ]
 
-EMPTY_VALUE = '-пусто-'
+EMPTY_VALUE = "-пусто-"
 
 MAX_USERNAME_LENGTH = 150
 MAX_EMAIL_LENGTH = 255
 MAX_SUBJECT_LENGTH = 255
 MAX_FORBIDDEN_WORLD_LENGTH = 100
+
+UNISENDER_API_KEY = os.getenv("UNISENDER_API_KEY")
+UNISENDER_SENDEMAIL_URL = "https://api.unisender.com/ru/api/sendEmail"
