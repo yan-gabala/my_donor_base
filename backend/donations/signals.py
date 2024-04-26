6e36efd042cd4df0dc33c7e38b8af78698db2f3c
jisdtn <122818010+jisdtn@users.noreply.github.com>
2024-04-26 12:44:16 +0200

    small fix signal

--- backend/donations/signals.py
+++ backend/donations/signals.py
@@ -1,13 +1,14 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-
-from . import send_failed_payment_email
-from .models import Donation
-
-
-@receiver(post_save, sender=Donation)
-def payment_rejected_handler(sender, instance, created, **kwargs):
-    if not created and instance.status == "rejected":
-        user_email = instance.user.email
-        message = f"Ваш платеж на {instance.amount} был отклонен."
-        send_failed_payment_email(user_email, message)
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
+#
+# from .utils import send_payment_email
+# from .models import Donation
+#
+#
+# @receiver(post_save, sender=Donation)
+# def payment_handler(sender, instance, created, **kwargs):
+#     if not created and instance.status == "rejected":
+# or another word status
+#         user_email = instance.email
+#         message = f"Ваш платеж на {instance.amount} был отклонен."
+#         send_payment_email(user_email, message)
