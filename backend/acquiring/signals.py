ac14b320080496b527ecc433947671e2ca27396c
jisdtn <122818010+jisdtn@users.noreply.github.com>
2024-04-29 11:43:17 +0200

    add acquiring app, move signals.py and utils.py to app

--- backend/acquiring/signals.py
+++ backend/acquiring/signals.py
@@ -8,7 +8,7 @@
 # @receiver(post_save, sender=Donation)
 # def payment_handler(sender, instance, created, **kwargs):
 #     if not created and instance.status == "rejected":
-# or another word status
+#                                                                 # or another word status # noqa: E501
 #         user_email = instance.email
 #         message = f"Ваш платеж на {instance.amount} был отклонен."
 #         send_payment_email(user_email, message)
