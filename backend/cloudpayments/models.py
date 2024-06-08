2acd603919df5fb4d7b17084676c48ef58b7b593
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-06-08 15:29:51 +0300

    Изменение обработки ответа Cloudpayments

--- backend/cloudpayments/models.py
+++ backend/cloudpayments/models.py
@@ -1,3 +1,7 @@
+# Модуль модели платежей Cloudpayments
+from django.conf import settings
+from django.db import models
+
 from donor_base.base_model import BaseModelDonation
 
 
@@ -6,7 +10,11 @@ class CloudPayment(BaseModelDonation):
     Модель платежа Cloudpayment.
     """
 
+    currency = models.CharField(
+        max_length=settings.MAX_CURRENCY_LENGTH, verbose_name="Валюта платежа"
+    )
+
     class Meta:
-        ordering = ("-pub_date", "payment_status")
+        ordering = ("-pub_date", "status")
         verbose_name = "Пожертвование Cloudpayment"
         verbose_name_plural = "Пожертвования Cloudpayment"
