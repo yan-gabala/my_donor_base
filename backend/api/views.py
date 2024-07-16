19309e18023069a6a7698c6156f36ac1dcb78155
Михаил Миноцкий <smoke-m@yandex.com>
2024-07-16 11:25:28 +0300

    egc

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,8 +26,8 @@ class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
-    @action(detail=False, url_path="get_cotacts", methods=("post",))
-    def get_cotacts(self, request):
+    @action(detail=False, url_path="get_contacts", methods=("post",))
+    def get_contacts(self, request):
         """Метод получения контактов от Unisender."""
         # проверка ручки, что живая из постман.
         return Response(dict(result="ok"), status=status.HTTP_200_OK)
