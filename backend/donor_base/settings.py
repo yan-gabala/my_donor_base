44bdf3e911cac9f54b5b2d6454e6d429e54e5430
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-07-17 14:31:19 +0300

    Настройка домена

--- backend/donor_base/settings.py
+++ backend/donor_base/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "api.apps.ApiConfig",
     "contacts.apps.ContactsConfig",
     "forbiddenwords.apps.ForbiddenwordsConfig",
