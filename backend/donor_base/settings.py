fad0c817eedb9659eccd429dddf9ae6eabbf1a74
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-07-18 12:00:51 +0300

    Настройка домена

--- backend/donor_base/settings.py
+++ backend/donor_base/settings.py
@@ -13,6 +13,8 @@ DEBUG = os.getenv("DEBUG", "").lower() in ["true", "yes", "1"]
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
