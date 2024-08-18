ed5ce2d58d2021c0f6777ee27bbb35c9941268df
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-08-18 18:05:18 +0300

    Проверка Celery

--- backend/donor_base/celery_config.py
+++ backend/donor_base/celery_config.py
@@ -16,6 +16,7 @@ django.setup()
 app = Celery("donor_base")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+app.conf.broker_connection_retry_on_startup = True
 
 
 @app.task(bind=True, ignore_result=True)
