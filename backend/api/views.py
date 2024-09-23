f93319f9ad892cc1f3f5ea9afa0ae426828b1ef5
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-09-23 16:45:13 +0300

    Изменение ответа вьюхи

--- backend/api/views.py
+++ backend/api/views.py
@@ -98,7 +98,7 @@ class CloudPaymentsViewSet(viewsets.GenericViewSet):
         )
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(dict(code=0), status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
